daf538a9e942deb357240e550b731533486d0a55
DevWesly <dev.lucaswesley@gmail.com>
2022-07-08 14:55:20 -0300

    criação das rotas

--- controllers/ContatosController.js
+++ controllers/ContatosController.js
@@ -1,5 +1,23 @@
+const uid = 1;
+
+
+
 module.exports = {
     listarContatos: (req,res)=>{
-        res.send('Trate logo se terminar esse curso pra ficar rico');
+        let contatos = require(`../database/contatos_${uid}.json`);
+        res.send(contatos);
+    },
+    capturarContato: (req,res)=>{
+        let contatos = require(`../database/contatos_${uid}.json`);
+        let idDoContato = req.params.id;
+        let contato = contatos.find(
+            (c)=>{
+                return c.id == idDoContato;
+            }
+        );
+        if(contato === undefined){
+            res.send({msg:"contato buscado ainda não existe"});
+        } else {res.send(contato);}
+
     }
 }
--- routes/ContatosRouter.js
+++ routes/ContatosRouter.js
@@ -5,4 +5,6 @@ const ContatosController = require('../controllers/ContatosController')
 const router = express.Router();
 router.get('/contatos',ContatosController.listarContatos);
 
+router.get('/contatos/:id', ContatosController.capturarContato);
+
 module.exports = router;
