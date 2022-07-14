b520303e0cdf32d591bc41d805b2abcc395a4e96
DevWesly <dev.lucaswesley@gmail.com>
2022-07-14 14:24:00 -0300

    criando cominho  UsuariosRouter e exibindo tela de registro

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
-const ContatosRouter = require('./routes/ContatosRouter')
+const ContatosRouter = require('./routes/ContatosRouter');
+const UsuariosRouter = require('./routes/UsuariosRouter');
 
 const app = express();
 
@@ -11,4 +12,8 @@ app.get('/',(req,res)=>{
 });
 
 app.use('/',ContatosRouter);
+app.use('/',UsuariosRouter);
+
+// app.use()
+
 app.listen(3300,()=>{console.log("Rodando porta 3300")});
