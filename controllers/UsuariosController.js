0f3c4916618798a79274c324ec01199d23496af8
DevWesly <dev.lucaswesley@gmail.com>
2022-07-31 15:47:39 -0300

    .....

--- controllers/UsuariosController.js
+++ controllers/UsuariosController.js
@@ -26,30 +26,49 @@ module.exports = {
 
         //determinar o novo id do novo usuarios
 
-        let idNovo = usuarios[usuarios.length-1].id + 1;
+        let idNovo = usuarios[usuarios.length - 1].id + 1;
         //criar a senha criptografada;
 
-        let senhaCrip = bcrypt.hashSync(senha,10);
+        let senhaCrip = bcrypt.hashSync(senha, 10);
 
         //criar um objeto com os dados do usuario
         let usuario = {
-            id:idNovo,
-            nome:nome,
-            email:email,
-            senha:senhaCrip
+            id: idNovo,
+            nome: nome,
+            email: email,
+            senha: senhaCrip
         };
 
         //adicionar o novo usuario a esse array--
-            usuarios.push(usuario);
+        usuarios.push(usuario);
 
         //Salvar esse array no arquivo usuarios.jsoon
-        fs.writeFileSync(path.join(__dirname,'/../database/usuarios.json'), JSON.stringify(usuarios,null,4));
+        fs.writeFileSync(path.join(__dirname, '/../database/usuarios.json'), JSON.stringify(usuarios, null, 4));
 
         //direcionando novo usuario      
         res.redirect('/contatos');
     },
     mostrarLogin: (req, res) => {
-        res.render('login.ejs');
+
+        let {
+            email,
+            senha
+        } = req.body;
+
+        const usuarios = require('../database/usuarios.json');
+
+        let usuario = usuarios.find(u => u.email == email && bcrypt.compareSync(senha, u.senha));
+
+
+
+
+
+
+
+
+        res.render('login.ejs', {
+            usuario
+        });
     },
     login: (req, res) => {
 
@@ -66,22 +85,46 @@ module.exports = {
         const usuarios = require('../database/usuarios.json');
 
         //verificar se o emais exite e se asenha deste email confere;
-        usuarios.find(
-            u => {
-                if (u.email == email && bcrypt.compareSync(senha, u.senha)) {
-                    return true;
 
-                } else {
-                    return false;
-                }
-            }
-        )
+
+        // let usuario = usuarios.find(
+        //     u => {
+        //         if (u.email == email && bcrypt.compareSync(senha, u.senha)) {
+        //             return true;
+
+        //         } else {
+        //             return false;
+        //         }
+        //     }
+        // );
+
+        let usuario = usuarios.find(u => u.email == email && bcrypt.compareSync(senha, u.senha));
+
+
 
         //se o usuario na for encontrado ou a senha for invalida, mandar erro
 
+        if (usuario == undefined) {
+
+            let usuario = 1;
+            return res.render("login.ejs", {
+                usuario,
+                email,
+                senha
+            })
+        };
+
+
+
         //se o usuario e senha ok; setar a sessopn do usuario
 
+        req.session.usuario = usuario;
+
+
+
         //e redirecionar usuario para a tela que lista contatos;
+
+        res.redirect('/contatos');
 
     }
 }
